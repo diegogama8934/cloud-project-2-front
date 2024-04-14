8a75454c57154950e054b05201fc24687c2b489d
Diego Martinez <diegogama8934@gmail.com>
2024-04-13 22:42:22 -0600

    Peticion a server local

--- components/Desarrollo.tsx
+++ components/Desarrollo.tsx
@@ -1,15 +1,35 @@
 "use client"
 import Image from "next/image";
 import { useState, useRef } from "react";
+import ImageMissing from '@/public/image-missing.svg';
+import { Button } from "@nextui-org/react";
 
 export const Desarrollo = () => {
-  const [urlImage, setUrlImage] = useState("");
-  const [imageReceived, setImageReceived] = useState("");
+  const [image, setImage] = useState(ImageMissing);
+  const [imageURL, setImageURL] = useState("");
+  const [imageReceived, setImageReceived] = useState(ImageMissing);
   const imageInputRef = useRef<HTMLInputElement>(null);
 
   const handleShowImage = () => {
     const file = imageInputRef.current?.files?.[0];
-    if (file) setUrlImage(URL.createObjectURL(file));
+    if (file) {
+      setImage(file);
+      setImageURL(URL.createObjectURL(file));
+    }
+  }
+
+  const handleSendImage = async () => {
+
+    const reqBody = new FormData();
+    reqBody.append("image", image);
+
+    const resBody = await fetch("http://localhost:5910/images", {
+      method: "POST",
+      body: reqBody
+    });
+
+    console.log(resBody);
+
   }
 
   return (
@@ -21,19 +41,20 @@ export const Desarrollo = () => {
           <h4 className="text-xl">Imagen a encriptar</h4>
           <div className="hover:cursor-pointer" onClick={() => imageInputRef.current?.click()}>
             <Image
-              src={urlImage ? urlImage : "/image-missing.svg"}
+              src={imageURL ? imageURL : ImageMissing}
               width={100}
               height={100}
               alt="Imagen inicial"
               className="rounded-xl aspect-square object-contain bg-default-50 shadow-2xl w-80"
             />
           </div>
+          <Button color="primary" onPress={handleSendImage}>Enviar</Button>
           <input className="hidden" type="file" accept="image/*" ref={imageInputRef} onChange={handleShowImage} />
         </div>
         <div className="flex flex-col gap-4 items-center">
           <h4 className="text-xl">Imagen desencriptada</h4>
           <Image
-            src={imageReceived ? imageReceived : "/image-missing.svg"}
+            src={imageReceived}
             width={100}
             height={100}
             alt="Imagen desencriptada"
